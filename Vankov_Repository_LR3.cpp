4006f564e0b2eb1a88455cb7c3b9713fcbd875d4
Alexander <vankoviks@gmail.com>
2025-03-14 12:57:53 +0300

    Create void main

---
+++ Vankov_Repository_LR3.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int a, b = 0;
+
+int CorrectInt(string message, int leftBorder=INT_MIN, int rightBorder=INT_MAX)
+{
+    int i;
+
+    cout << message;
+
+    while (!(cin >> i) || cin.peek() != '\n' || i < leftBorder || i >= rightBorder)
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        cout << message;
+    }
+
+    return i;
+}
+
+// Function to set value of variable "a"
+void EnterA()
+{
+    // Must be developed by Petrov P. P.
+}
+
+// Function to set value of variable "b"
+void EnterB()
+{
+    // Must be developed by Ivanov I. I.
+}
+
+// Function to sum up variables "a" and "b"
+void ExecutePlus()
+{
+    // Must be developed by Kuznetsov K. K.
+}
+
+// Function to minus variables "a" and "b"
+void ExecuteMinus()
+{
+    // Must be developed by Tarakanov T. T.
+}
+
+int main()
+{   
+    int choice;
+
+    for (;;)
+    {
+        cout << "Task 1: Enter A" << endl;
+        cout << "Task 2: Enter B" << endl;
+        cout << "Task 3: Execute \"+\"" << endl;
+        cout << "Task 4: Execute \"-\"\n" << endl;
+
+        choice = CorrectInt("Enter the number of task or 0 to exit: ", 0, 5);
+
+        switch (choice)
+        {
+            case 0:
+                return 0;
+            
+            case 1:
+                EnterA();
+                break;
+            
+            case 2:
+                EnterB();
+                break;
+            
+            case 3:
+                ExecutePlus();
+                break;
+
+            case 4:
+                ExecuteMinus();
+                break;
+        }
+    }
+}
